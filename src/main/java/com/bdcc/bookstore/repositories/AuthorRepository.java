7a89d4d0a56b9d9a58bb208359a6720f39e257ac
Youssef Achir <43423207+Meihern@users.noreply.github.com>
2021-01-28 20:43:21 +0100

    - Updated kubernetes deployment configuration bookstore-deployment-config.yaml:   - Adding loadbalancer service   - Adding bookstore-service-config.yaml configuration in the same file - Removed the bookstore-service-config.yaml file

--- src/main/java/com/bdcc/bookstore/repositories/AuthorRepository.java
+++ src/main/java/com/bdcc/bookstore/repositories/AuthorRepository.java
@@ -2,7 +2,6 @@ package com.bdcc.bookstore.repositories;
 
 import com.bdcc.bookstore.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
--- src/main/java/com/bdcc/bookstore/repositories/BookRepository.java
+++ src/main/java/com/bdcc/bookstore/repositories/BookRepository.java
@@ -2,8 +2,6 @@ package com.bdcc.bookstore.repositories;
 
 import com.bdcc.bookstore.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 
 public interface BookRepository extends JpaRepository<Book, Long>{
 
